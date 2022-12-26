94356e0d305d40048f778865bc98ad34e9f2e83d
Clarice0997 <clarice09975@gmail.com>
2022-12-26 19:44:01 +0800

    feat: bcrypt加密 注册账号密码加密

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,12 +3,18 @@ package com.example.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @MapperScan("com.example.demo.mapper")
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+    @Bean
+    public BCryptPasswordEncoder bcryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
 
--- src/main/java/com/example/demo/service/LoginService.java
+++ src/main/java/com/example/demo/service/LoginService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import com.example.demo.entity.Account;
 import com.example.demo.mapper.LoginMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -10,8 +11,17 @@ import org.springframework.stereotype.Service;
 public class LoginService {
     @Autowired
     private LoginMapper loginMapper;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public String register(Account account){
+        // 获取密码
+        String password = account.getPassword();
+        // 加密密码
+        password = bCryptPasswordEncoder.encode(password);
+        // 重新赋值加密密码
+        account.setPassword(password);
+
         int i = loginMapper.register(account);
         if(i!=0){
             return "注册成功";
